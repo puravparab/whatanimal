afad6b442525d3e2db71b2d4f80747bf5cb0f67c
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-08-14 05:13:31 +0400

    uploaded image preview added

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Resizer from "react-image-file-resizer";
 
 import logo from '../assets/images/android-chrome-512x512.png'
 import '../styles/components/input.css'
@@ -10,14 +11,20 @@ const Input = () => {
 		setInputImage(logo)
 	}, [])
 
+	const imageHandler = (e) => {
+		setInputImage(URL.createObjectURL(e.target.files[0]))
+	}
+
 	return (
 		<div className="input-container">
 			<div className="image-container">
-				<img src={inputImage} />
+				<img src={inputImage} alt="user input" />
 			</div>
 			<div className="image-detail">
 				<label for="input-image">Add an image of an animal:</label>
-				<input type="file" id="avatar" name="input-image" accept="image/*" />
+				<input type="file" id="image-input" name="input-image" accept="image/*" 
+					onChange={imageHandler}
+				/>
 			</div>
 		</div>
 	)
