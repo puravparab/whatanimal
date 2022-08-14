9bc280ca3b13defaf05b9a15738d008a9968e964
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-08-14 20:24:27 +0400

    style update to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
-import logo from '../assets/images/android-chrome-512x512.png'
+import { useNavigate } from 'react-router-dom'
 
+import logo from '../assets/images/android-chrome-512x512.png'
 import '../styles/components/header.css'
 
 const Header = () => {
+	let navigate = useNavigate()
+
 	return (
 		<div className="header">
 			<div className="header-left">
-				<img src={logo} alt="cat logo" />
+				<img src={logo} alt="cat logo" onChange={()=>{
+					navigate('/')
+				}} />
 				<h1 className="title">
 					WhatAnimal?
 				</h1>
