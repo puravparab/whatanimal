02aa5ae3ac85db1941562fc36600a2b30044d525
PuravParab <47409476+PuravParab@users.noreply.github.com>
2022-08-15 01:15:25 +0400

    added a api endpoint to post user uploaded image to the server

--- server/model/urls.py
+++ server/model/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
 	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
+	path('analyze', get_image, name='analyze')
 ]
--- server/model/views.py
+++ server/model/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser, MultiPartParser
+from rest_framework.decorators import api_view, parser_classes
 
-# Create your views here.
+@api_view(["POST"])
+@parser_classes([MultiPartParser])
+def get_image(request):
+	data = request.data
+	image = data.get("image")
+
+	return Response({"message": "succesful"}, status=status.HTTP_200_OK)
